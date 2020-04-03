f99f4af8f9c46a9c982a355f5848e81d20267318
zhupengcheng <>
2020-04-03 07:23:20 +0000

    修改

--- src/main/java/com/lezhi/controller/UserController.java
+++ src/main/java/com/lezhi/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.lezhi.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,18 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.fastjson.JSONObject;
 import com.lezhi.dto.UserDto;
 import com.lezhi.entity.ResultBean;
+import com.lezhi.entity.User;
 import com.lezhi.service.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@Autowired
-	UserService UserService;
-	
+	UserService userService;
+
 	@PostMapping("/login")
-	public ResultBean<Integer> login(@RequestBody String params){
+	public ResultBean<Integer> login(@RequestBody String params, HttpServletRequest request) {
 		UserDto userDto = JSONObject.parseObject(params, UserDto.class);
-		 return new ResultBean<>(UserService.login(userDto));
+		
+		Integer ret = userService.login(userDto);
+		HttpSession session = request.getSession();
+		
+		User user = new User();
+		user.setUserName(userDto.getUserName());
+		session.setAttribute("user", user);
+		session.setMaxInactiveInterval(60);
+		
+		return new ResultBean<>(ret);
 	}
 }
